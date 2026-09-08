3a8bd8574070a12282ab3e00efb25eae6c6119b5
Amulya <nallaamulya043@gmail.com>
2026-09-08 10:13:04 +0530

    registration & login

---
+++ authproject/accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render,redirect
+from .forms import RegistrationForm
+# Create your views here.
+
+
+
+
+
+
+def home(request):
+    return render(request,'accounts/home.html')
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    else:
+        form = RegistrationForm()
+    return render(request,'accounts/register.html',{'form': form })
+
+# loginView
